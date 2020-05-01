ad00e5737d4fab8aa5a0040c1b9409f47476ccd3
Leonard Morrison <lmohamed1@studentmail.nait.ca>
2020-05-01 00:37:14 -0600

    Refactored the names of the buttons and started working on the Java Class for the Main Activity.

--- app/src/main/java/com/coshopcode/coshopapp/MainActivity.java
+++ app/src/main/java/com/coshopcode/coshopapp/MainActivity.java
@@ -3,12 +3,38 @@ package com.coshopcode.coshopapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView title = findViewById(R.id.title);
+    CircleImageView imageView = findViewById(R.id.circleImageView);
+    TextView tagline = findViewById(R.id.tagline);
+    Button startingSignupButton = findViewById(R.id.startingSignup);
+    Button startingLoginButton = findViewById(R.id.startinglogin);
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    public void onClickStartingSignupButton(View view)
+    {
+        //TODO: Move the Activity to the SignUp Page upon Login
+    }
+
+    public void onClickStartingLoginButton(View view)
+    {
+        //TODO: Transfer to the Login Page.
+    }
+
+
 }
