52a0ca262f4b636dcf6e6972b380b7f784f9bce5
Leonard Morrison <lmohamed1@studentmail.nait.ca>
2020-05-05 16:25:27 -0600

    Added Firebase Database functionality

--- app/src/main/java/com/coshopcode/coshopapp/LoginActivity.java
+++ app/src/main/java/com/coshopcode/coshopapp/LoginActivity.java
@@ -4,11 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //Initalize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
     }
+
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+
+
 }
