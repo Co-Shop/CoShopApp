014fb401bc5de288c36f899231774b6ee79e7ba4
Leonard Morrison <lmohamed1@studentmail.nait.ca>
2020-05-20 01:16:12 -0600

    Created the RegisterUser method

--- app/src/main/java/com/coshopcode/coshopapp/SignUpActivity.java
+++ app/src/main/java/com/coshopcode/coshopapp/SignUpActivity.java
@@ -2,19 +2,42 @@ package com.coshopcode.coshopapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
-public class SignUpActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignUpActivity extends AppCompatActivity
+{
+    private FirebaseAuth mFirebaseAuth;
+    EditText emailText = findViewById(R.id.emailEntry);
+    EditText signUpPasswordText = findViewById(R.id.signupPassword);
+    EditText signUpConfirmPasswordText = findViewById(R.id.signUpPasswordConfirm);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        mFirebaseAuth = FirebaseAuth.getInstance();
     }
 
-    public void onClickSignupToSignIn(View view)
+    public void onClickSignUpToLogIn(View view)
     {
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void onClickRegisterUser(View view)
+    {
+        String email = emailText.getText().toString();
+        String password = signUpPasswordText.getText().toString();
+        String confirmPassword = signUpConfirmPasswordText.getText().toString();
+
+
 
     }
 }
