4e35fed9189e0f5da1639d9f3864861667cee1fc
Leonard Morrison <lmohamed1@studentmail.nait.ca>
2020-05-25 11:14:53 -0600

    Implementing E-mail Verification on this Sign Up page

--- app/src/main/java/com/coshopcode/coshopapp/SignUpActivity.java
+++ app/src/main/java/com/coshopcode/coshopapp/SignUpActivity.java
@@ -1,12 +1,18 @@
 package com.coshopcode.coshopapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpActivity extends AppCompatActivity
@@ -15,13 +21,19 @@ public class SignUpActivity extends AppCompatActivity
     EditText emailText = findViewById(R.id.emailEntry);
     EditText signUpPasswordText = findViewById(R.id.signupPassword);
     EditText signUpConfirmPasswordText = findViewById(R.id.signUpPasswordConfirm);
+    ProgressBar progressBar = findViewById(R.id.signUpProgressBar);
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        progressBar.setVisibility(View.INVISIBLE);
         mFirebaseAuth = FirebaseAuth.getInstance();
+        if(mFirebaseAuth.getCurrentUser() != null)
+        {
+            finish();
+        }
     }
 
     public void onClickSignUpToLogIn(View view)
@@ -33,9 +45,45 @@ public class SignUpActivity extends AppCompatActivity
 
     public void onClickRegisterUser(View view)
     {
-        String email = emailText.getText().toString();
-        String password = signUpPasswordText.getText().toString();
-        String confirmPassword = signUpConfirmPasswordText.getText().toString();
+        progressBar.setVisibility(View.VISIBLE);
+        final String email = emailText.getText().toString();
+        final String password = signUpPasswordText.getText().toString();
+        final String confirmPassword = signUpConfirmPasswordText.getText().toString();
+        if(!password.equals(confirmPassword))
+        {
+            Toast.makeText(this, "The password and the confirmation do not match!", Toast.LENGTH_LONG);
+        }
+        mFirebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task)
+            {
+                progressBar.setVisibility(View.GONE);
+                if(task.isSuccessful())
+                {
+                    mFirebaseAuth.getCurrentUser().sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task)
+                        {
+                            if(task.isSuccessful())
+                            {
+                                Toast.makeText(SignUpActivity.this, "Registration has been successful.", Toast.LENGTH_LONG).show();
+                                emailText.setText("");
+                                signUpPasswordText.setText("");
+                                signUpConfirmPasswordText.setText("");
+                            }
+
+                            else
+                            {
+                                Toast.makeText(SignUpActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                            }
+
+
+                        }
+                    });
+                }
+
+            }
+        });
 
 
 
