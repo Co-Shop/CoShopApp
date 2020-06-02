9e7bacf3b016d41de2b6f033fbe94c7336bcc71a
Leonard Morrison <lmohamed1@studentmail.nait.ca>
2020-06-02 02:27:08 -0600

    Adding the API URL and the methods to connect to the Internet. Also added a check for Internet Connection

--- app/src/main/java/com/coshopcode/coshopapp/MainActivity.java
+++ app/src/main/java/com/coshopcode/coshopapp/MainActivity.java
@@ -2,7 +2,15 @@ package com.coshopcode.coshopapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.net.NetworkInfo;
+import android.net.NetworkRequest;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,10 +21,15 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import de.hdodenhof.circleimageview.CircleImageView;
+import okhttp3.OkHttp;
 
 public class MainActivity extends AppCompatActivity {
    TextView tagline= findViewById(R.id.tagline);
-   String URL = "";
+   String URL = "http://api.coshop.org/";
+   //TODO: Add the URL and use the Rest API to Log In and Register.
+   private ConnectivityManager cManager;
+   private ConnectivityManager.NetworkCallback mCallback;
+   private AlertDialog mDialog;
 
 
 
@@ -25,9 +38,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+
         //Fixing the Tagline textview
         tagline.setElegantTextHeight(true);
         tagline.setSingleLine(false);
+
+
+
 
         //FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
         //FirebaseUser user = firebaseAuth.getCurrentUser();
@@ -42,7 +61,22 @@ public class MainActivity extends AppCompatActivity {
 
     private void ConnectToTheInternet()
     {
+       if(isNetworkAvailable())
+       {
 
+       }
+       else
+       {
+
+       }
+
+    }
+
+    private boolean isNetworkAvailable()
+    {
+        cManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        return cManager.isDefaultNetworkActive();
     }
 
     public void onClickStartingSignupButton(View view)
