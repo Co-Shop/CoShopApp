8729b9c2dfb7fa9c5594621d21a35392d6fa93bb
Leonard Morrison <lmohamed1@studentmail.nait.ca>
2020-06-03 01:45:54 -0600

    Added an Internet Check to the MainActivity. I'll start work on the tests soon.

--- app/src/main/java/com/coshopcode/coshopapp/MainActivity.java
+++ app/src/main/java/com/coshopcode/coshopapp/MainActivity.java
@@ -1,20 +1,24 @@
 package com.coshopcode.coshopapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.Network;
 import android.net.NetworkCapabilities;
 import android.net.NetworkInfo;
 import android.net.NetworkRequest;
+import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -59,25 +63,47 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void ConnectToTheInternet()
+    ConnectivityManager.NetworkCallback callback = new ConnectivityManager.NetworkCallback()
     {
-       if(isNetworkAvailable())
-       {
+        @Override
+        public void onAvailable(@NonNull Network network)
+        {
+            //
+            // Toast.makeText(this, "Welcome to the CoshopApp", Toast.LENGTH_SHORT).show();
 
-       }
-       else
-       {
+        }
 
-       }
+        @Override
+        public void onLost(@NonNull Network network) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle("No Internet").setMessage("You need to have Internet Access in order to access this application.").setNegativeButton("I don't care, kick me out!", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                }
+            }).setPositiveButton("Connect me to the Internet", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+                    try {
+                        wifiManager.wait(100, 300);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
 
+                }
+
+            });
+
+        }
+    };
+
+    private void kickUserOut()
+    {
+        finish();
     }
 
-    private boolean isNetworkAvailable()
-    {
-        cManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
 
-        return cManager.isDefaultNetworkActive();
-    }
 
     public void onClickStartingSignupButton(View view)
     {
