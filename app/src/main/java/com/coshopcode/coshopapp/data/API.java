e83ae7229ce4d1905038cbd834314ce822dd76cb
Leonard Morrison <lmohamed1@studentmail.nait.ca>
2020-06-05 15:31:19 -0600

    Created the API interface and Model class

---
+++ app/src/main/java/com/coshopcode/coshopapp/data/API.java
@@ -0,0 +1,10 @@
+package com.coshopcode.coshopapp.data;
+
+public interface API
+{
+    void registerUser(String email, String password);
+
+    void loginUser(String email, String password);
+
+
+}
---
+++ app/src/main/java/com/coshopcode/coshopapp/data/Model.java
@@ -0,0 +1,11 @@
+package com.coshopcode.coshopapp.data;
+
+import com.google.android.gms.common.api.Api;
+
+public class Model
+{
+    public void login(String email, String password)
+    {
+
+    };
+}
