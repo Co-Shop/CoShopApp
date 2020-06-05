ce0ce2c8bafb270241fd4bc9693dceea7e16c6ac
Leonard Morrison <lmohamed1@studentmail.nait.ca>
2020-06-05 13:58:33 -0600

    Created the User class

---
+++ app/src/main/java/com/coshopcode/coshopapp/UserData/User.java
@@ -0,0 +1,11 @@
+package com.coshopcode.coshopapp.UserData;
+
+public class User
+{
+    private String mUserName;
+    private String mEmail;
+    private String mPassword;
+    private String mConfirmPassword;
+
+
+}
