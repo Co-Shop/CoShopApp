8d392cc9fb5e2a201d6f6a26adfad38dd4555e52
Leonard Morrison <lmohamed1@studentmail.nait.ca>
2020-06-25 14:52:08 -0600

    Added a Request Queue to the Model

--- app/src/main/java/com/coshopcode/coshopapp/data/API.java
+++ app/src/main/java/com/coshopcode/coshopapp/data/API.java
@@ -11,4 +11,5 @@ public interface API
     void updatePassword(String password);
 
 
+
 }
--- app/src/main/java/com/coshopcode/coshopapp/data/Model.java
+++ app/src/main/java/com/coshopcode/coshopapp/data/Model.java
@@ -1,9 +1,14 @@
 package com.coshopcode.coshopapp.data;
 
+import com.android.volley.RequestQueue;
 import com.google.android.gms.common.api.Api;
+
 
 public class Model implements API
 {
+    public static final String BASE_URL = "http://api.coshop.org/";
+
+    private RequestQueue requestQueue;
 
     @Override
     public void registerUser(String email, String password) {
